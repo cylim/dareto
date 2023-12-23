14ada93e30c0018b7e4cdcdfdfe1a08aff810c26
cylim <cylim226@gmail.com>
2023-12-23 20:19:40 +0800

    add alchemy aa

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,11 +1,22 @@
 import React, { createContext, useEffect, useState } from "react";
 import { User, authSubscribe } from "@junobuild/core-peer";
+import {
+  LightSmartContractAccount,
+  getDefaultLightAccountFactoryAddress,
+} from "@alchemy/aa-accounts";
+import { AlchemyProvider } from "@alchemy/aa-alchemy";
+import { LocalAccountSigner, type Hex } from "@alchemy/aa-core";
+import { sepolia } from "viem/chains";
+import { ALCHEMY_API_KEY } from "@/config/env";
+import { toHex } from 'viem'
 
-export const AuthContext = createContext<{user: User | null}>({user: null});
+const chain = sepolia;
+
+export const AuthContext = createContext<{ user: User | null, provider: AlchemyProvider | null }>({ user: null, provider: null});
 
 export const Auth = ({ children }: {children: React.ReactElement[] | React.ReactElement }) => {
   const [user, setUser] = useState<User | null>(null);
-
+  const [provider, setProvider] = useState<AlchemyProvider | null>(null)
   useEffect(() => {
     const unsub = authSubscribe((user) => setUser(user));
 
@@ -13,12 +24,36 @@ export const Auth = ({ children }: {children: React.ReactElement[] | React.React
   }, []);
 
   useEffect(() => {
+    if(!user) { 
+      setProvider(null)
+      return 
+    }
     console.log(user)
+
+    const PRIVATE_KEY = toHex((user?.key || '0x').slice(0,30), {size: 32}) as Hex;
+    const owner = LocalAccountSigner.privateKeyToAccountSigner(PRIVATE_KEY);
+
+    const provider = new AlchemyProvider({
+      apiKey: ALCHEMY_API_KEY,
+      chain,
+    }).connect(
+      (rpcClient) =>
+        new LightSmartContractAccount({
+          rpcClient,
+          owner,
+          chain,
+          factoryAddress: getDefaultLightAccountFactoryAddress(chain),
+        })
+    );
+
+    (async () => {
+      console.log("Smart Account Address: ", await provider.getAddress());
+    })();
 
   }, [user])
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, provider }}>
       {children}
     </AuthContext.Provider>
   );
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,1 +1,2 @@
 export const SATELITE_ID = process.env.NEXT_PUBLIC_JUNO_SATELITE || ''
+export const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || ''
